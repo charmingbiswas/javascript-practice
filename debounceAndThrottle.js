4262ce35ad9d0c2029602ee6964bb04fc1864cbc
Riyansh Biswas <charmingbiswas@gmail.com>
2023-12-06 00:55:51 +0530

    Implemented debounce and throttling functions

--- debounceAndThrottle.js
+++ debounceAndThrottle.js
@@ -1,13 +1,19 @@
 
 const debounce = (func, delay) => {
     let timer;
-
-    return () => {
-
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), delay)
     }
 }
 
 
 const throttle = (func, delay) => {
-
+    let wait = false;
+    return (...args) => {
+        if(wait) return;
+        func.apply(this, args);
+        wait = true;
+        setTimeout(() => wait = false, delay);
+    }
 }
